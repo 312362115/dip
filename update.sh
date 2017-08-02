cce63669499e7a4eee1d676b3fd704b859bfd457
renlongyu <312362115@qq.com>
2017-08-02 23:21:17 +0800

    update at 2017-08-02 23:21:17

--- update.sh
+++ update.sh
@@ -4,7 +4,9 @@
 cd  "$(dirname "$0")"
 
 
-curl cip.cc | awk -F : 'NR==1{print $2}' > ip.txt
+# curl cip.cc | awk -F : 'NR==1{print $2}' > ip.txt
+dig +short myip.opendns.com @resolver1.opendns.com > ip.txt
+
 
 
 if [ -z "$(git st --porcelain)"]; then
