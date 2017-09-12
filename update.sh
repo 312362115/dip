5a96081440bbbe37285fbd0184e176cd5a2b4e8b
312362115 <312362115@qq.com>
2017-09-12 22:13:59 +0800

    更新脚本

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ cd  "$(dirname "$0")"
 
 if [ -n "$(git cherry origin/master master -v)" ]; then
     git push origin master
+fi
 
 # curl ip.cip.cc > ip.txt
 dig +short myip.opendns.com @resolver1.opendns.com > ip.txt
